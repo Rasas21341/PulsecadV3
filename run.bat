bfe119c00fdc147caac225e043b8b0c1feacc368
Rasas21341 <oceanviewrp1@gmail.com>
2026-07-12 16:58:31 -0400

    Remove run.bat script for PulseCAD Desktop App

--- run.bat
+++ run.bat
@@ -1,15 +1,1 @@
-@echo off
-REM PulseCAD Desktop App - Run Script
-REM This script starts the Electron app in development mode
 
-echo Starting PulseCAD Desktop App...
-echo.
-
-REM Install dependencies if needed
-if not exist node_modules (
-    echo Installing dependencies...
-    call npm install
-)
-
-REM Start the app
-call npm start
