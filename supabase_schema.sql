49b0fb21e83434296a5bc8b5978a8aca869e9fc0
Rasas21341 <oceanviewrp1@gmail.com>
2026-08-17 22:46:43 -0400

    Update supabase_schema.sql

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -1,1 +1,4 @@
+ALTER TABLE communities 
+ADD COLUMN IF NOT EXISTS supervisor_password TEXT;
 
+COMMENT ON COLUMN communities.supervisor_password IS 'Password required to access the community dashboard/settings.';
